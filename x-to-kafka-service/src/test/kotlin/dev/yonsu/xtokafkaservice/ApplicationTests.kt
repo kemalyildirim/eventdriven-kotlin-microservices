8e0f9b8980ed67e127f81562009fd735d6c47b2a
Kemal Yildirim <kemalyildirm@gmail.com>
2024-08-31 17:52:02 +0300

    init

---
+++ x-to-kafka-service/src/test/kotlin/dev/yonsu/xtokafkaservice/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yonsu.xtokafkaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
